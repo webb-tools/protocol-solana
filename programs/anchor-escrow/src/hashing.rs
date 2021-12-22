c88e7c5831ecfa62a014836692444d5de41a4fbe
Drew Stone <drew@drews-mbp.myfiosgateway.com>
2021-12-22 15:42:57 -0500

    Update hash processing

--- programs/anchor-escrow/src/hashing.rs
+++ programs/anchor-escrow/src/hashing.rs
@@ -4,15 +4,52 @@ use ark_ff::{BigInteger, PrimeField};
 use ark_std::{marker::PhantomData, vec::Vec};
 use arkworks_gadgets::poseidon::CRH;
 use arkworks_utils::poseidon::PoseidonParameters;
-use arkworks_utils::utils::bn254_x5_3::{FULL_ROUNDS, PARTIAL_ROUNDS, WIDTH, SBOX, get_mds_poseidon_bn254_x5_3, get_rounds_poseidon_bn254_x5_3};
+use arkworks_utils::utils::bn254_x5_3::{
+    FULL_ROUNDS,
+    PARTIAL_ROUNDS,
+    WIDTH,
+    SBOX,
+    get_mds_poseidon_bn254_x5_3,
+    get_rounds_poseidon_bn254_x5_3,
+    ROUND_CONSTS,
+    MDS_ENTRIES
+};
+use ark_bn254::Fr as Bn254;
+use arkworks_utils::utils::{get_bytes_array_from_hex};
+
 pub struct CircomPoseidonHasher<F: PrimeField>(PhantomData<F>);
+
+fn parse_vec<F: PrimeField>(arr: Vec<&str>) -> Vec<F> {
+	let mut res = Vec::new();
+    let mut ctr = 0;
+	for r in arr.iter() {
+        msg!("{}", ctr);
+		let c = F::from_be_bytes_mod_order(&get_bytes_array_from_hex(r));
+		res.push(c);
+        ctr += 1;
+	}
+	res
+}
+
+pub fn parse_matrix<F: PrimeField>(mds_entries: Vec<Vec<&str>>) -> Vec<Vec<F>> {
+	let width = mds_entries.len();
+	let mut mds: Vec<Vec<F>> = vec![vec![F::zero(); width]; width];
+	for i in 0..width {
+		for j in 0..width {
+			// TODO: Remove unwrap, handle error
+			mds[i][j] = F::from_be_bytes_mod_order(&get_bytes_array_from_hex(mds_entries[i][j]));
+		}
+	}
+	mds
+}
+
 
 impl<F: PrimeField> CircomPoseidonHasher<F> {
     pub fn hash(input: &[u8]) -> Result<Vec<u8>, Error> {
         msg!("before rounds");
-        let rounds = get_rounds_poseidon_bn254_x5_3();
+        let rounds = parse_vec(ROUND_CONSTS.to_vec());
         msg!("before mds");
-        let mds = get_mds_poseidon_bn254_x5_3();
+        let mds = parse_matrix(MDS_ENTRIES.iter().map(|x| x.to_vec()).collect::<Vec<_>>());
         msg!("before params");
         let params = PoseidonParameters::<F>::new(rounds, mds, FULL_ROUNDS, PARTIAL_ROUNDS, WIDTH, SBOX);
         msg!("before output");
@@ -22,5 +59,4 @@ impl<F: PrimeField> CircomPoseidonHasher<F> {
     }
 }
 
-use ark_bn254::Fr as Bn254;
 pub type BN254CircomPoseidon3x5Hasher = CircomPoseidonHasher<Bn254>;
